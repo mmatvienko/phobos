d6120fcd53f253499457161e6069a2db9837922c
Marc Matvienko <m.matvienko@utexas.edu>
2020-06-16 20:53:04 -0500

    actually functioning utils tests

---
+++ test/test_utils.py
@@ -0,0 +1,31 @@
+import unittest
+import datetime
+import pytest
+
+import pandas as pd
+
+from ..utils import set_test_env, pull_historical_data
+
+set_test_env()
+
+class TestPullHistorical(unittest.TestCase):
+    def test_pull(self):
+        ticker = "GOOG"
+        start = pd.Timestamp("2019-01-01")
+        end = pd.Timestamp("2019-02-01")
+
+        df = pull_historical_data(ticker, start, end)
+        assert len(df) == 22
+
+        end_ = pd.Timestamp("2019-03-01")
+        df = pull_historical_data(ticker, start, end_)
+        assert len(df) == 41
+
+        df = pull_historical_data(ticker,pd.Timestamp("2019-01-15"), end)
+        assert len(df) == 13
+
+    def test_errors(self):
+        with pytest.raises(ValueError):
+            pull_historical_data("GOOG", "2019-01-01", pd.Timestamp("2019-01-01"))
+        with pytest.raises(ValueError):
+            pull_historical_data("GOOG", pd.Timestamp("2019-01-01"), "2019-01-01")
