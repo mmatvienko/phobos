e463fdc15816f39c9a4d4c9654e5787c4b3fa4cd
Marc Matvienko <m.matvienko@utexas.edu>
2020-12-06 20:04:04 +0100

    introduced basic runner for SMA strategy

--- pegasus/local_runner.py
+++ pegasus/local_runner.py
@@ -1,4 +1,43 @@
-from canopus.utils import pull_historical_data
-from titan.portfolio import Portfolio
+import pandas as pd
+import trading_calendars as tc
+import logging, sys
 
-p = Portfolio()
+from pegasus.strategies.sma import SMA
+from titan.portfolio import Portfolio
+from canopus.utils import set_test_env
+
+
+def run():
+    strat = SMA(Portfolio(cash=100_000))
+    nasdaq = tc.get_calendar("NASDAQ")
+
+    final_date = None
+    for date in pd.date_range(start=pd.Timestamp('2019-01-01'), periods=100, freq="1D"):
+
+        if not nasdaq.is_session(date):
+            logging.info(f"Skipping {date}")
+            continue
+        logging.info(f'Running on {date}')
+
+        
+        strat.run(timestamp=date)
+        final_date = date
+
+    print(strat.portfolio.evaluate(date=final_date))
+    print(strat.portfolio.cash)
+if __name__ == "__main__":
+    set_test_env()
+
+    # setup logging logic
+    file_handler = logging.FileHandler(filename='tmp.log')
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    handlers = [file_handler, stdout_handler]
+    logging.basicConfig(
+        level=logging.DEBUG, 
+        format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+        handlers=handlers
+    )
+
+    logger = logging.getLogger('LOGGER_NAME')
+    
+    run()
--- pegasus/strategies/sma.py
+++ pegasus/strategies/sma.py
@@ -1,19 +1,42 @@
 from time import time
+from titan.security import Security
+from titan.portfolio import Portfolio
+
+import logging
+import pandas as pd
 
 class SMA():
-    def __init__(self, portfolio, tickers=["SPY"]):
+    def __init__(self, portfolio, tickers=["SPY"], run_name:str="sma"):
         """ SMA will buy and sell based on SMA 50 vs 200 cross
         Will do it for all tickers. Could use metaflow step on each tickers
         A strategy is the operation of some trades at a single tiem stamp
         The runner is what will handle running it at repeated periods
         """
         self.portfolio = portfolio
+        self.run_name = run_name
+        self.tickers = tickers
 
-    def run(self, timestamp=None):
-        for tick in tickers:
-            # TODO
-  
-        return None
+    def run(self, timestamp: pd.Timestamp=None):
+        """  Given some time, run the strategy at that time.
+        if timestamp is None, run live
+
+        """
+        for tick in self.tickers:
+            sec = Security(tick)
+            short_days = 50
+            short_sma = sec.get_sma("daily", short_days, timestamp=timestamp)
+            long_days = 200
+            long_sma = sec.get_sma("daily", long_days, timestamp=timestamp)
+            
+            logging.info(f"SMA{short_days} = {short_sma}\tSMA{long_sma} = {long_sma}")
+            # if sma 50 < sma 200, sell
+            if short_sma > long_sma:
+                # go short
+                self.portfolio.close_pos(tick, timestamp=timestamp)
+
+            elif short_sma < long_sma:
+                # go long
+                self.portfolio.order(tick, 10, timestamp=timestamp)
 
     def backfill(self, timestamp=None):
         """ Backfill all needed data for running step at timestamp
